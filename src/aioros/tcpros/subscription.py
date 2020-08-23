8a794bd3a0f9b9f76eb68a1789c787875a770d53
Markus Grimm <grimm.markus@gmail.com>
2020-08-23 20:56:09 +0200

    Typo and slots

--- src/aioros/tcpros/subscription.py
+++ src/aioros/tcpros/subscription.py
@@ -1,5 +1,7 @@
 class Subscription:
 
+    __slots__ = ('_manager', '_topic', 'callback')
+
     def __init__(self, manager, topic, callback):
         self._manager = manager
         self._topic = topic
@@ -7,7 +9,7 @@ class Subscription:
 
     @property
     def topic_name(self):
-        return self.topic.name
+        return self._topic.name
 
     async def close(self) -> None:
         self._manager.unregister_subscription(self)
