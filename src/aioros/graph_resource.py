f3499370812adda8e0667d55232559f7ff7f1b94
Markus Grimm <grimm.markus@gmail.com>
2020-09-18 20:41:05 +0200

    Simplify graph name resolutions

--- src/aioros/graph_resource.py
+++ src/aioros/graph_resource.py
@@ -2,26 +2,35 @@ import os
 import re
 import sys
 
-from .names import canonicalize_name
-from .names import is_global
-from .names import is_private
-from .names import make_global_ns
-from .names import ns_join
+from typing import Pattern
+
 from .remappings import Remappings
 
 
-NS_PATTERN = re.compile(r'^__ns:=(?P<ns>.*)')
-MASTER_PATTERN = re.compile(r'^__master:=(?P<master>.+)')
-ADDRESS_PATTERN = re.compile(r'^__(ip|hostname):=(?P<address>.+)')
+NS_PATTERN: Pattern = re.compile(r'^__ns:=(?P<ns>.*)')
+MASTER_PATTERN: Pattern = re.compile(r'^__master:=(?P<master>.+)')
+ADDRESS_PATTERN: Pattern = re.compile(r'^__(ip|hostname):=(?P<address>.+)')
 
 
 def _get_ros_namespace() -> str:
     for arg in sys.argv:
         match = NS_PATTERN.match(arg)
         if match:
-            return match.groupdict()['ns']
+            name = match.groupdict()['ns']
+            break
+    else:
+        name = os.environ.get('ROS_NAMESPACE', '/')
 
-    return make_global_ns(os.environ.get('ROS_NAMESPACE', '/'))
+    if name.startswith('~'):
+        raise ValueError('Cannot resolve private names')
+
+    if not name.startswith('/'):
+        name = '/' + name
+
+    if not name.endswith('/'):
+        name += '/'
+
+    return name
 
 
 def get_master_uri() -> str:
@@ -51,7 +60,7 @@ class GraphResource:
 
     def __init__(self, node_name: str) -> None:
         self._namespace: str = _get_ros_namespace()
-        self._node_name: str = ns_join(self._namespace, node_name)
+        self._node_name: str = self._namespace + node_name
         self._remappings = Remappings()
 
     @property
@@ -65,12 +74,13 @@ class GraphResource:
     def resolve(self, name: str) -> str:
         if not name:
             return self._namespace
-        name = canonicalize_name(name)
-        if is_global(name):
-            resolved_name = name
-        elif is_private(name):
-            resolved_name = ns_join(self._node_name, name[1:])
+
+        canonical_name = '/'.join(i for i in name.split('/') if i)
+        if name.startswith('/'):
+            resolved_name = '/' + canonical_name
+        elif canonical_name.startswith('~'):
+            resolved_name = self._node_name + '/' + canonical_name[1:]
         else:
-            resolved_name = self._namespace + name
+            resolved_name = self._namespace + canonical_name
 
         return self._remappings.get(resolved_name, resolved_name)
